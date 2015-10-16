1136b5227d423846aa83657016b69a835321b9de
Kinarki <nickfriend81@gmail.com>
2015-10-15 18:26:37 -0700

    Memory Match v0.5 - Nick Friend

--- myjs.js
+++ myjs.js
@@ -1,12 +1,30 @@
 var firstClicked = null;
+var secondClicked = null;
 
+function cardClick(card_element) {
+    $(card_element).addClass('hidden');
+    var the_card = $(card_element).prev().attr('src');
 
+        if(firstClicked == null) {
 
-function cardClick(cardNumber) {
-    var the_card = $('#card' + cardNumber).attr('src');
-    if(firstClicked == null) {
-        firstClicked = the_card;
-    } else if(firstClicked == the_card) {
+            firstClicked = the_card;
+            console.log(firstClicked);
 
-    }
+        } else {
+
+            secondClicked = the_card;
+
+            if(firstClicked == secondClicked) {
+                //hide cards
+                console.log('match');
+
+                firstClicked = null;
+                secondClicked = null;
+
+            } else {
+                //reset
+                firstClicked = null;
+                secondClicked=null;
+            }
+        }
 }
