f189ed7c55cba2beb0151668b676eb36feec76f6
Kinarki <nickfriend81@gmail.com>
2015-10-19 12:18:05 -0700

    Memory Match v1.0

--- myjs.js
+++ myjs.js
@@ -1,7 +1,78 @@
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 2;
-var match_counter = 0;
+var total_possible_matches = 7;
+var matches = 0;
+var attempts =  0;
+var accuracy = 0;
+var games_played = 0;
+
+
+//randomize cards
+$(function () {
+
+    var parent = $('#game-area');
+
+    var divs = parent.children();
+
+    while(divs.length){
+        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    }
+});
+
+//calculate accuracy
+function accuracyScore() {
+    var accuracy = (matches/ attempts) * 100;
+
+    accuracy = accuracy.toFixed(2);
+
+    return accuracy + '%';
+}
+
+//display stats when called
+function display_stats() {
+
+    $('.games-played .value').html(" " + games_played);
+
+    $(".attempts .value").html(" " + attempts);
+
+    $('.accuracy .value').html(" " +  accuracyScore());
+}
+
+//reset statistics
+function reset_stats () {
+
+    accuracy = 0;
+    matches = 0;
+    attempts = 0;
+
+    display_stats();
+
+}
+
+// reset button
+function reset(){
+
+    games_played++;
+
+    $('.front').removeClass('hidden');
+
+    $('.back').removeClass('hidden');
+
+    $(function () {
+
+        var parent = $('#game-area');
+
+        var divs = parent.children();
+
+        while(divs.length){
+            parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+        }
+    });
+
+    reset_stats();
+
+    //display_stats();
+}
 
 //function to check for card matches
 function card_clicked(card_element) {
@@ -21,15 +92,34 @@ function card_clicked(card_element) {
         return;
 
     } else {
-
         //saving second card for comparison
         second_card_clicked = the_card;
+
+        attempts++;
+
 
         //checking for match
         if (first_card_clicked == second_card_clicked) {
 
-            //incrementing match_counter for win condition comparison
-            match_counter++;
+            if(second_card_clicked == 'cardZ') {
+                alert('Game over.  You\'ve been turned!');
+                setTimeout(function() {
+                    $('.front').removeClass('hidden');
+
+                    $('.back').removeClass('hidden');
+
+                    reset();
+                },1500);
+
+            }
+
+
+            //disable mouse clicks on the game board while two cards are face up
+            //works on all browsers save Opera Mini
+            $('#game-area').css('pointer-events', 'none');
+
+            //incrementing matches for win condition comparison
+            matches++;
 
             //allot time for player to see that they got a match
             setTimeout(function () {
@@ -37,13 +127,24 @@ function card_clicked(card_element) {
                 //hides matched pairs
                 $("[card_src='" + the_card + "']").addClass('hidden');
 
+                //enabling mouse clicks on the game board
+                $('#game-area').removeAttr('style');
+
             }, 500);
 
             //win condition comparison
-            if (match_counter == total_possible_matches) {
+            if (matches == total_possible_matches) {
 
                 //eric hates this
                 alert('You win!');
+
+                setTimeout(function() {
+                    $('.front').removeClass('hidden');
+
+                    $('.back').removeClass('hidden');
+
+                    reset();
+                },1500);
 
             }
             //resetting variables for next round of match finding.
@@ -56,6 +157,7 @@ function card_clicked(card_element) {
             //disable mouse clicks on the game board while two cards are face up
             //works on all browsers save Opera Mini
             $('#game-area').css('pointer-events', 'none');
+
 
             //function to reset cards
             setTimeout(function () {
@@ -74,7 +176,8 @@ function card_clicked(card_element) {
                 $('#game-area').removeAttr('style');
 
                 return;
-            }, 2000);
+            }, 1250);
         }
+        display_stats();
     }
 }
